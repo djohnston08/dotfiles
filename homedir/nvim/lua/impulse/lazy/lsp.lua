9da2101c965a4ca1c7647e9b4c7df090ee0a726b
David Johnston <davidjohnston05@gmail.com>
2024-04-30 00:00:55 -0400

    add new go to definition in a split config

--- homedir/nvim/lua/impulse/lazy/lsp.lua
+++ homedir/nvim/lua/impulse/lazy/lsp.lua
@@ -128,6 +128,7 @@ return {
                 local opts = { buffer = ev.buf }
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gdv', ':vsplit | lua vim.lsp.buf.definition()<CR>', opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
