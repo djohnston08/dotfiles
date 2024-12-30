85a51f31c3a8ed9e87333d6d0c448b477368c961
David Johnston <davidjohnston05@gmail.com>
2024-12-29 23:41:43 -0500

    Update git keymaps for neogit

--- homedir/nvim/lua/impulse/lazy/git.lua
+++ homedir/nvim/lua/impulse/lazy/git.lua
@@ -3,20 +3,68 @@ return {
     {
         'lewis6991/gitsigns.nvim',
         config = function()
-            require('gitsigns').setup()
-            vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
-            vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
-            vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
-            vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
-            vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
-            vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ ']c', bang = true })
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end)
+
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ '[c', bang = true })
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end)
+
+                    -- Actions
+                    map('n', '<leader>hs', gitsigns.stage_hunk)
+                    map('n', '<leader>hr', gitsigns.reset_hunk)
+                    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('n', '<leader>hS', gitsigns.stage_buffer)
+                    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+                    map('n', '<leader>hR', gitsigns.reset_buffer)
+                    map('n', '<leader>hp', gitsigns.preview_hunk)
+                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                    map('n', '<leader>hd', gitsigns.diffthis)
+                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+                    map('n', '<leader>td', gitsigns.toggle_deleted)
+
+                    -- Text object
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            }
+
+            -- require('gitsigns').setup()
+            -- vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+            -- vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+            -- vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+            -- vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+            -- vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+            -- vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+            -- vim.keymap.set('n', 'gB', ':Gitsigns blame_line<CR>')
         end,
     },
     -- 'tpope/vim-rhubarb',
     -- {
     --     'tpope/vim-fugitive',
     --     config = function()
-    --     vim.cmd [[
+    --         vim.cmd [[
     --         nnoremap <leader>vs :G<CR>
     --         nnoremap <leader>vc :G commit<CR>
     --         nnoremap <leader>vp :G push<CR>
