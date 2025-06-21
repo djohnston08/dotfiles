c3456f1f1d5099c19a2742a245138eb6163773ed
David Johnston <davidjohnston05@gmail.com>
2025-06-21 11:08:37 -0400

    feat: Add markdown preview plugin for Neovim
    - Add iamcco/markdown-preview.nvim for live markdown preview in browser
    - Configure with <leader>mp keybinding to toggle preview
    - Set auto_close to 0 and theme to dark

---
+++ homedir/nvim/lua/impulse/lazy/markdown.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		keys = {
+			{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+		},
+		config = function()
+			vim.g.mkdp_auto_close = 0
+			vim.g.mkdp_theme = "dark"
+		end,
+	},
+}
